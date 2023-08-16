694cd9ce7c9a1c39ec4aa78a4e3850c35809cbfc
DESKTOP-E2HCUV1\admin <liqiming@inbody.com>
2023-08-16 18:58:49 +0800

    commit

---
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class HelloController {
+
+    @GetMapping
+    public String hello(){
+        return "hello wms";
+    }
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/list")
+    public List<User> list(){
+//        return userService.list();
+        return userService.listAll();
+    }
+}
